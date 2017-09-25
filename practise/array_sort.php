1aece42e53b72bce8038dfa9cd7eb7ed288879fa
ranjun <ranjun@wondershare.cn>
2017-09-25 16:21:03 +0800

    add array sort

---
+++ practise/array_sort.php
@@ -0,0 +1,20 @@
+<?php  
+$cars=array("Volvo","BMW","Toyota");
+var_dump($cars);
+echo "<br>";
+sort($cars);
+var_dump($cars);
+echo "<br>";
+$phones=array("iPhone","vivi","oppo","huawei","xiaomi");
+var_dump($phones);
+echo "<br>";
+sort($phones);
+var_dump($phones);
+
+echo "<br>";
+$numbers=array(4,6,2,22,11);
+var_dump($numbers);
+echo "<br>";
+sort($numbers);
+var_dump($numbers);
+?>
