b3c2113c51cb76f1c6276911b8cca7d467527803
ranjun <ranjun@wondershare.cn>
2017-09-25 15:48:12 +0800

    add array num association

---
+++ practise/array_num_association.php
@@ -0,0 +1,8 @@
+<?php  
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+echo "Peter is " . $age['Peter'] . " years old.";
+echo "<br>";
+echo "Ben is " . $age['Ben'] . " years old.";
+echo "<br>";
+echo "Joe is " . $ae['Joe'] . " years old";
+?>
