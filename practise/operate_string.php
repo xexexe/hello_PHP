72f69b1d0f691929aeb36a208f2bc913bedbfcf6
ranjun <ranjun@wondershare.cn>
2017-09-25 14:45:02 +0800

    add operate string

---
+++ practise/operate_string.php
@@ -0,0 +1,15 @@
+<?php  
+$a = "Hello";
+$b = $a . " world!";
+echo $b; // 输出Hello world! 
+echo "<br>";
+ 
+$x="Hello";
+$x .= " world!";
+echo $x; // 输出Hello world! 
+echo "<br>";
+
+$o = "o";
+$o .= "_o";
+echo $o;
+?>
