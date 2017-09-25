5a221e18518e385311816c479f13455d30be17c4
ranjun <ranjun@wondershare.cn>
2017-09-25 15:30:28 +0800

    add array length and num

---
+++ practise/array_length.php
@@ -0,0 +1,8 @@
+<?php  
+$cars=array("Volvo","BMW","Toyota");
+echo count($cars);
+
+echo "<br>";
+$phones=array("iPhone","vivi","oppo","huawei","xiaomi");
+echo count($phones);
+?>
---
+++ practise/array_num.php
@@ -0,0 +1,8 @@
+<?php  
+$cars=array("Volvo","BMW","Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+echo "<br>";
+$phones=array("iPhone","vivi","oppo","huawei","xiaomi");
+echo "I like " . $phones[0] . "," . $phones[1] . "," . $phones[2] . "," . $phones[3] . "," . $phones[4] . ".";
+?>
